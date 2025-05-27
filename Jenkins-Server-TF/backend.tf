d234316dad5520ef8c5fcd3b79f51f848d4ef251
Than Toe Aung <toe842005@gmail.com>
2025-05-27 13:11:35 +0630

    rename s3 and dynamodb table

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "devsecops-jenkins-terraform"
     region         = "ap-southeast-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    dynamodb_table = "terraform-locks"
     encrypt        = true
   }
   required_version = ">=0.13.0"
